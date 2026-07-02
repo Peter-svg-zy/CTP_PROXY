8b4a04129dce24bfb8d97469796740a8ccff87e3
Peter-svg-zy <2940504501@qq.com>
2026-07-02 15:38:47 +0800

    增加功能测试文件

---
+++ 功能测试单元/Global_Param.py
@@ -0,0 +1,101 @@
+import configparser
+import json
+from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
+# 账户信息
+# simnow
+import pandas as pd
+#
+# broker_name = 'simnow模拟'
+# investorID = ''
+# password = ''
+# broker_id = '9999'
+# market_server_front = 'tcp://182.254.243.31:30011'
+# trade_server_front = 'tcp://182.254.243.31:30001'
+# appID = 'simnow_client_test'
+# authcode = '0000000000000000'
+# productInfo_fileName = './con_file/productInfo.ini'
+
+broker_name = 'simnow模拟7*24小时'
+investorID = ''
+password = ''
+broker_id = '9999'
+market_server_front = 'tcp://182.254.243.31:30011'
+trade_server_front = 'tcp://182.254.243.31:40001'
+appID = 'simnow_client_test'
+authcode = '0000000000000000'
+productInfo_fileName = './con_file/productInfo.ini'
+
+# broker_name = 'ws'
+# investorID = '02'
+# password = '156'
+# broker_id = '9999'
+# market_server_front = 'ws://127.0.0.1:5253'
+# trade_server_front = 'tcp://180.168.146.187:10201'
+# appID = 'simnow_client_test'
+# authcode = '0000000000000000'
+# productInfo_fileName = './con_file/productInfo.ini'
+
+
+# 其他信息
+subID = []
+# 查询信号，用于检查是否查询完成，true代表正在查询
+qry_flag = True
+
+# 下单必要元素
+frontID = None  # 前置编号
+sessionID = None  # 会话编号
+maxOrderRef = None  # 最大报单引用
+
+# 策略字典
+strategy_map = {}
+# 订单字典
+order_map = {}
+# 持仓字典
+position_map = {}
+# 详细持仓字典
+positionDetail_map = {}
+
+# 交易日
+tradingDay = None
+
+# 线程池
+save_data_pool = ThreadPoolExecutor(5)
+
+# 其他
+tduserapi = None
+tduserspi = None
+
+mduserapi = None
+mduserspi = None
+
+# 合约交易所
+ExchangeID = {}
+
+# 合约手续费和保证金等
+productInfo = configparser.ConfigParser()
+productInfo.read(productInfo_fileName, encoding='utf-8')
+
+# 读取json文件
+try:
+    with open('../con_file/ExchangeID.json', 'r', encoding='utf8') as f:
+        ExchangeID = json.load(f)
+# 如果有错误则创建文件
+except Exception as e:
+    print('读取ExchangeID.json失败！\n失败原因：{}\n已创建空白文件'.format(e))
+    with open('../con_file/ExchangeID.json', 'w', newline='\n', encoding='utf-8') as f:
+        # json.dump(ExchangeID, f, ensure_ascii=False)
+        data = json.dumps(ExchangeID, indent=4, ensure_ascii=False)
+        f.write(data)
+
+# 数据队列
+dataQueue = Queue()
+
+# 计算数据传送时间
+start = None
+end = None
+
+
+
+
+
---
+++ 功能测试单元/login_md.py
@@ -0,0 +1,66 @@
+from CTP_API import thostmduserapi as tdapi
+from CTP_API import thostmduserapi as mdapi
+import Global_Param as g
+from function import *
+
+"""纯行情登录实现功能"""
+
+#创建回调接口spi
+class CFtdcMdSpi(mdapi.CThostFtdcMdSpi):
+    def __init__(self, mduserapi):
+        mdapi.CThostFtdcMdSpi.__init__(self)
+        self.mduserapi =mduserapi
+    # 当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用，可以在该方法内实现用户登录
+    def OnFrontConnected(self):
+        loginfield = mdapi.CThostFtdcReqUserLoginField()
+        loginfield.BrokerID = g.broker_id
+        loginfield.UserID = g.investorID
+        loginfield.Password =g.password
+
+        ret= self.mduserapi.ReqUserLogin(loginfield,0)   #在这里调用用户登录接口，调用用户登录接口后触发其回调接口
+        if ret == 0:
+            print('发送用户登录行情请求成功')
+        else:
+            print('发送用户登录行情账户请求失败！')
+            judge_ret(ret)
+
+    def OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast):   # 用户登录的回调接口
+        """pRspUserLogin:结构体，包含交易日，登录时间
+           pRspInfo:响应信息结构体包含ErrorId和 ErrorMsg，None登录成功
+           nRequestID:用户操作请求ID，这里就是上方ret填入参数的0
+           bIsLast:指示该次返回是否为针对nRequestID的最后一次返回
+        """
+        if pRspInfo.ErrorID != 0 and pRspInfo != None:
+            print('行情连接失败\n错误信息为：{}\n错误代码为：{}'.format(pRspInfo.ErrorMsg, pRspInfo.ErrorID))
+        else:
+            print('行情账户登录成功！')
+            # g.subID = ["FG266", "SA266", 'au2609', 'sc2609']
+            # # 订阅行情
+            # ret = self.mduserapi.SubscribeMarketData([id.encode('utf-8') for id in g.subID],
+            #                                          len(g.subID))
+            # if ret == 0:
+            #     print('发送订阅合约请求成功！')
+            # else:
+            #     print('发送订阅合约请求失败！')
+            #     judge_ret(ret)
+
+
+class CTP_MdLogin(object):
+    def __init__(self, **kwargs):
+        pass
+    def connect_to_md(self):
+        self.mduserapi=mdapi.CThostFtdcMdApi_CreateFtdcMdApi('../con_file/')  # 创建api实例
+        self.mduserspi=CFtdcMdSpi(self.mduserapi)   # spi实例
+        g.mduserapi=self.mduserapi
+        g.mduserspi=self.mduserspi
+            # 登录行情前置服务器
+        self.mduserapi.RegisterFront(g.market_server_front)
+            # 将spi注册给api
+        self.mduserapi.RegisterSpi(self.mduserspi)
+            # 第5步，API正式启动，dll底层会自动去连上面注册的地址
+        self.mduserapi.Init()
+        self.mduserapi.Join()
+
+if __name__ == '__main__':
+    ctp_login = CTP_MdLogin()
+    ctp_login.connect_to_md()
